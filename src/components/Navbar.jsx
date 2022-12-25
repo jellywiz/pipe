4fc3f71e352d1baee7918c9744b6720f0f161863
jellywiz <muzaffer.dler@gmail.com>
2022-12-25 14:54:09 +0300

    add navbar and header files and style

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './Navbar.css';
+import logo from '../img/logo-1-removebg-preview.png';
+
+function Navbar() {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <a href="#home">
+            <li>Home</li>
+          </a>
+          <a href="#about">
+            <li>About</li>
+          </a>
+          <a href="#service">
+            <li>Services</li>
+          </a>
+          <a href="#about-us">
+            <li>About Us</li>
+          </a>
+        </ul>
+        <div className="header-logo">
+          <img src={logo} alt="" />
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
