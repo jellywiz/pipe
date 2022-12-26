23f43e333bd6b428448e121fe3240560f2157e44
jellywiz <muzaffer.dler@gmail.com>
2022-12-26 11:00:32 +0300

    Add about page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import Header from './components/Header';
 import Navbar from './components/Navbar';
+import About from './components/About';
+import Slideshow from './components/Slideshow';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Navbar />
+      <About />
+      <Slideshow />
       <h1>nice</h1>
     </div>
   );
