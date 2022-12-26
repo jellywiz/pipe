267bc662567e844d072ca7ba9d405d2161e7c194
jellywiz <muzaffer.dler@gmail.com>
2022-12-26 11:39:17 +0300

    Adding the slideshow

--- src/App.js
+++ src/App.js
@@ -5,12 +5,17 @@ import About from './components/About';
 import Slideshow from './components/Slideshow';
 
 function App() {
+  const images = [
+    'https://picsum.photos/id/10/400/300',
+    'https://picsum.photos/id/100/400/300',
+    'https://picsum.photos/id/1000/400/300',
+  ];
   return (
     <div className="App">
       <Header />
       <Navbar />
       <About />
-      <Slideshow />
+      <Slideshow images={images} interval={3000} />
       <h1>nice</h1>
     </div>
   );
--- src/components/Slideshow.jsx
+++ src/components/Slideshow.jsx
@@ -1,8 +1,43 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/img-redundant-alt */
+import React, { useState, useEffect } from 'react';
 import './Slideshow.css';
 
-function Slideshow() {
-  return <div>Slideshow</div>;
+function Slideshow({ images, interval }) {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [selectedIndex, setSelectedIndex] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentIndex((currentIndex + 1) % images.length);
+    }, interval);
+    return () => clearInterval(intervalId);
+  }, [currentIndex, images.length, interval]);
+
+  useEffect(() => {
+    setSelectedIndex(currentIndex);
+  }, [currentIndex]);
+
+  const selectImage = (index) => {
+    setSelectedIndex(index);
+    setCurrentIndex(index);
+  };
+
+  return (
+    <div className="slideshow">
+      <img src={images[currentIndex]} alt="slideshow image" />
+      <div className="slideshow-circles">
+        {images.map((image, index) => (
+          <div
+            key={image}
+            className={`slideshow-circle ${
+              index === selectedIndex ? 'selected' : ''
+            }`}
+            onClick={() => selectImage(index)}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Slideshow;
